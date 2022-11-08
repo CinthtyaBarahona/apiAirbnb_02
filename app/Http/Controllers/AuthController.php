a0bb890efdbdc4c822b985936fff764b5440c84b
Cinthya Barahona <noemybarahona24@gmail.com>
2022-11-08 16:50:20 -0600

    Registrar administrador

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -113,4 +113,29 @@ class AuthController extends Controller
             'user' => $user
         ], 201);
     }
+
+    protected function registerAdmin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(),400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validate(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        $user->assignRole('admin');
+
+        return response()->json([
+            'message' => 'Usuario Administrador registrado correctamente!!',
+            'user' => $user
+        ], 201);
+    }
 }

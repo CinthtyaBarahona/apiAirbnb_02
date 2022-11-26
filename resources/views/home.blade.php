09b12d7f5a337e63c3f431bf733ed533eb503e17
Cinthya Barahona <noemybarahona24@gmail.com>
2022-11-26 10:06:52 -0600

    Agregando vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,8 @@
       crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
       rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"/>
-      <link rel="stylesheet" href="{{ asset('css/inicio.css') }}" />
-      <title>Airbnb</title>
+    <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+    <title>Airbnb</title>
 
 </head>
 <body>
@@ -46,7 +46,7 @@
 
     <div class="content-inicio">
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -54,7 +54,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -62,7 +62,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -70,7 +70,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -78,7 +78,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -86,7 +86,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
@@ -94,7 +94,7 @@
         </div>
 
         <div class="card d-flex flex-column justify-content-between ml-2">
-            <img src="{{ asset('img/lugar.jpg') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('images/lugar.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title">LUGAR 1</h5>
                <a href="#" class="btn btn-primary btn-block">Ver Lugar</a>
